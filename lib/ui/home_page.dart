3fef4aae87832061c9ec2c1ce0d67450631de634
brlib4m <bruno.liborio21@gmail.com>
2020-11-10 21:57:40 -0400

    Att

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -42,16 +42,41 @@ class _HomePageState extends State<HomePage> {
             child: TextField(
               decoration: InputDecoration(
                   labelText: 'Search Here!',
-                  labelStyle: TextStyle(color:
-                  ),
+                  labelStyle: TextStyle(color: Colors.white),
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
             ),
-          )
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch(snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if(snapshot.hasError) return Container();
+                    else _createGifTable(context, snapshot);
+                }
+              },
+            ),
+          ),
         ],
       ),
     );
   }
-}
 
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+    return Grid
+  }
+}
